5c5b264248cfc42d3d32098731f878edde6c3c81
Nevercomes <sunss1903@163.com>
2020-08-01 00:22:06 +0800

    ready for common problems

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,80 +1,131 @@
 <template>
   <q-page>
-    <div class="q-pa-md">
-      <q-infinite-scroll
-        @load="onLoad"
-        :offset="250"
-        :disable="noMore"
-      >
-        <div
-          v-for="(item, index) in items"
-          :key="index"
-          class="caption"
-        >
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum repellendus sit voluptate voluptas eveniet porro. Rerum blanditiis perferendis totam, ea at omnis vel numquam exercitationem aut, natus minima, porro labore.</p>
-        </div>
-        <template v-slot:loading>
-          <div class="row justify-center q-my-md">
-            <q-spinner-dots
-              color="primary"
-              size="40px"
-            />
-          </div>
-        </template>
-        <div v-if="noMore">没有更多了</div>
-      </q-infinite-scroll>
-    </div>
+    <div
+      class="q-pa-md"
+      style="height: 400px; width: 100vw"
+      id="mapBox"
+    ></div>
   </q-page>
 </template>
 
 <script>
+
+const echarts = require('echarts/lib/echarts')
+require('src/assets/china')
+
+import { getMapData } from 'src/api/statistic'
+
 export default {
   name: 'PageIndex',
   data () {
+    const sqlData = ['北京市', '天津市', '上海市', '重庆市', '河北省', '河南省',
+      '云南省', '辽宁省', '黑龙江省', '湖南省', '安徽省', '山东省', '新疆', '江苏省',
+      '浙江省', '江西省', '湖北省', '广西', '甘肃省', '山西省', '内蒙古',
+      '陕西省', '吉林省', '福建省', '贵州省', '广东省', '青海省', '西藏', '四川省',
+      '宁夏', '海南省', '台湾省', '香港', '澳门'
+    ]
+    const mapData = ['北京', '天津', '上海', '重庆', '河北', '河南',
+      '云南', '辽宁', '黑龙江', '湖南', '安徽', '山东', '新疆', '江苏',
+      '浙江', '浙江', '江西', '湖北', '广西', '甘肃', '山西', '内蒙古',
+      '陕西', '吉林', '福建', '贵州', '广东', '青海', '西藏', '四川',
+      '宁夏', '海南', '台湾', '香港', '澳门'
+    ]
     return {
-      items: [],
-      noMore: false,
-      count: 0,
-      firstLoading: false
+      sqlData: sqlData,
+      mapData: mapData,
+      chart: {}
     }
   },
   mounted () {
-    // this.$q.notify({
-    //   message: '操作成功',
-    //   color: 'negative'
-    // })
-    this.msgInfo('你好')
+    const myChart = this.initMap()
+    getMapData().then(res => {
+      const data = res.data
+      const myData = []
+      let maxNumber = -1
+      data.forEach(item => {
+        if (item.number > maxNumber) { maxNumber = item.number }
+        myData.push({
+          name: this.nameMap(item.province),
+          value: item.number
+        })
+      })
+      myChart.hideLoading()
+      myChart.setOption({
+        series: [{
+          name: '数据',
+          type: 'map',
+          mapType: 'china',
+          roam: true,
+          label: {
+            normal: {
+              show: false // 省份名称,显示的话太挤了
+            },
+            emphasis: {
+              show: false
+            }
+          },
+          data: myData // 数据
+        }],
+        visualMap: {
+          min: 0,
+          max: maxNumber + 300,
+          x: 'left',
+          y: 'center',
+          text: ['High', 'Low'],
+          realtime: false,
+          calculable: true,
+          inRange: {
+            color: ['lightskyblue', 'yellow', 'orangered']
+          }
+        }
+      })
+    })
   },
   methods: {
-    alert () {
-      this.$q.dialog({
-        title: 'Alert',
-        message: 'Some message'
-      }).onOk(() => {
-        console.log('OK')
-      }).onCancel(() => {
-        console.log('Cancel')
-      }).onDismiss(() => {
-        // console.log('I am triggered on both OK and Cancel')
-      })
-    },
-    refresh (done) {
-      setTimeout(() => {
-        this.items.push({}, {}, {}, {}, {}, {}, {})
-        done()
-      }, 1000)
-    },
+    initMap () {
+      // 初始化echarts实例
+      var myChart = echarts.init(document.getElementById('mapBox'))
+      myChart.showLoading()
 
-    onLoad (index, done) {
-      setTimeout(() => {
-        if (this.items) {
-          this.items.push({}, {}, {}, {}, {}, {}, {})
-          done()
+      var optionMap = {
+        backgroundColor: '#FFFFFF',
+        title: {
+          // text: this.getYear() + '年报名学生生源地各省分布',
+          subtext: '截止时间 ' + this.getTime(),
+          x: 'center'
+        },
+        tooltip: {
+          trigger: 'item'
         }
-      }, 2000)
-      this.count++
-      if (this.count === 3) this.noMore = true
+      }
+
+      // 使用制定的配置项和数据显示图表
+      myChart.setOption(optionMap)
+      return myChart
+    },
+    // sql存储的省市名称到map josn数据的转化
+    nameMap (sqlName) {
+      for (const i in this.sqlData) {
+        if (this.sqlData[i] === sqlName) {
+          return this.mapData[i]
+        }
+      }
+      return sqlName
+    },
+    getTime () {
+      const today = new Date()
+      const year = today.getFullYear()
+      const month = today.getMonth() + 1
+      const day = today.getDate()
+      const hour = today.getHours()
+      const min = today.getMinutes()
+      const sec = today.getSeconds()
+      return year + '-' + month + '-' + day + ' ' + hour + ':' + min + ':' + sec
+    },
+    getYear () {
+      return new Date().getFullYear()
     }
   }
+
 }
 </script>
--- src/pages/SignUpStatistic.vue
+++ src/pages/SignUpStatistic.vue
@@ -130,6 +130,11 @@
       id="map"
       :title="'全国校区招生地图'"
     />
+    <div
+      class="q-pa-md"
+      style="height: 360px; width: 100vw"
+      id="mapBox"
+    />
 
     <nl-block-title
       id="school"
@@ -272,6 +277,9 @@
 
 <script>
 
+const echarts = require('echarts/lib/echarts')
+require('src/assets/china')
+
 import NlBlockTitle from 'src/components/NlBlockTitle'
 
 import { scroll, date } from 'quasar'
@@ -309,15 +317,29 @@ export default {
     }
   },
   data () {
+    const sqlProvince = ['北京市', '天津市', '上海市', '重庆市', '河北省', '河南省',
+      '云南省', '辽宁省', '黑龙江省', '湖南省', '安徽省', '山东省', '新疆', '江苏省',
+      '浙江省', '江西省', '湖北省', '广西', '甘肃省', '山西省', '内蒙古',
+      '陕西省', '吉林省', '福建省', '贵州省', '广东省', '青海省', '西藏', '四川省',
+      '宁夏', '海南省', '台湾省', '香港', '澳门'
+    ]
+    const mapProvince = ['北京', '天津', '上海', '重庆', '河北', '河南',
+      '云南', '辽宁', '黑龙江', '湖南', '安徽', '山东', '新疆', '江苏',
+      '浙江', '浙江', '江西', '湖北', '广西', '甘肃', '山西', '内蒙古',
+      '陕西', '吉林', '福建', '贵州', '广东', '青海', '西藏', '四川',
+      '宁夏', '海南', '台湾', '香港', '澳门'
+    ]
     return {
+      sqlProvince: sqlProvince,
+      mapProvince: mapProvince,
+
       // tab的激活项
-      tab: 'map',
+      tab: 'statistic',
 
       // 实体数据
       yesterdaySum: [],
       todaySum: [],
       yearSum: [],
-      mapData: [],
       schoolData: [],
       provinceData: [],
       ktProData: [],
@@ -346,9 +368,6 @@ export default {
     getYearSignSum().then(res => {
       this.yearSum = res.data
     })
-    getMapData().then(res => {
-      this.mapData = res.data
-    })
     getSchoolData().then(res => {
       this.schoolData = res.data
     })
@@ -364,6 +383,51 @@ export default {
 
     getPros(this.shopId).then(res => {
       this.proList = res.data
+    })
+  },
+  mounted () {
+    const myChart = this.initMap()
+    getMapData().then(res => {
+      const data = res.data
+      const myData = []
+      let maxNumber = -1
+      data.forEach(item => {
+        if (item.number > maxNumber) { maxNumber = item.number }
+        myData.push({
+          name: this.nameMap(item.province),
+          value: item.number
+        })
+      })
+      myChart.hideLoading()
+      myChart.setOption({
+        series: [{
+          name: '数据',
+          type: 'map',
+          mapType: 'china',
+          roam: true,
+          label: {
+            normal: {
+              show: false // 省份名称,显示的话太挤了
+            },
+            emphasis: {
+              show: false
+            }
+          },
+          data: myData // 数据
+        }],
+        visualMap: {
+          min: 0,
+          max: 10000,
+          x: 'left',
+          y: 'center',
+          text: ['High', 'Low'],
+          realtime: false,
+          calculable: true,
+          inRange: {
+            color: ['lightskyblue', 'yellow', 'orangered']
+          }
+        }
+      })
     })
   },
   methods: {
@@ -388,6 +452,49 @@ export default {
       const offset = el.offsetTop - el.scrollHeight
       const duration = 1000
       setScrollPosition(target, offset, duration)
+    },
+    initMap () {
+      // 初始化echarts实例
+      var myChart = echarts.init(document.getElementById('mapBox'))
+      myChart.showLoading()
+
+      var optionMap = {
+        backgroundColor: '#FFFFFF',
+        title: {
+          // text: this.getYear() + '年报名学生生源地各省分布',
+          subtext: '截止时间 ' + this.getTime(),
+          x: 'center'
+        },
+        tooltip: {
+          trigger: 'item'
+        }
+      }
+
+      // 使用制定的配置项和数据显示图表
+      myChart.setOption(optionMap)
+      return myChart
+    },
+    // sql存储的省市名称到map josn数据的转化
+    nameMap (sqlName) {
+      for (const i in this.sqlProvince) {
+        if (this.sqlProvince[i] === sqlName) {
+          return this.mapProvince[i]
+        }
+      }
+      return sqlName
+    },
+    getTime () {
+      const today = new Date()
+      const year = today.getFullYear()
+      const month = today.getMonth() + 1
+      const day = today.getDate()
+      const hour = today.getHours()
+      const min = today.getMinutes()
+      const sec = today.getSeconds()
+      return year + '-' + month + '-' + day + ' ' + hour + ':' + min + ':' + sec
+    },
+    getYear () {
+      return new Date().getFullYear()
     }
   }
 }
